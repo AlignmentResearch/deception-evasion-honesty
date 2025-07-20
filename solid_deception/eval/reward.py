a518d53cc2185cd0646ae91deb899847f3f61225
rrmaura <rrmaura@gmail.com>
2025-07-20 04:39:00 +0000

    Update data sampling in reward evaluation to allow replacement during subsampling
    - Mostly to avoid issues with small subsamples during debugging

--- solid_deception/eval/reward.py
+++ solid_deception/eval/reward.py
@@ -821,7 +821,7 @@ def main(args):
     print(f"Found {len(df)} entries")
     if args.n_rows is not None:
         print(f"Subsampling to {args.n_rows} entries")
-        df_sample = df.sample(n=args.n_rows, random_state=42)
+        df_sample = df.sample(n=args.n_rows, random_state=42, replace=True)
     else:
         df_sample = df
     # Remove debug_training effect on evaluation samples - dataset size should be controlled by debug_frac in data munging
