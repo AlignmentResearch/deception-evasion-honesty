3aac453a8cf7703b0c33fc83297844bc13d45553
rrmaura <rrmaura@gmail.com>
2025-07-20 11:42:08 +0100

    Update per device batch size in iterative training script to avoid memory errors

--- run_iterative.sh
+++ run_iterative.sh
@@ -48,7 +48,7 @@ export DO_BT_RM=true # Bradley-Terry reward model
 export DO_CATEGORICAL_RM=false
 export ADAPTIVE=false
 export RESTART_GRPO=false
-export BASE_PDTBS=64 # Per device batch size for 8b
+export BASE_PDTBS=32 # Per device batch size for 8b
 
 # Model
 export GENERATION_LORA_PATH=None
